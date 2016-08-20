4644b4bfc831ca6f3af1fc7d5a98391b02bdf427
brainyfarm <brainyfarm@gmail.com>
2016-08-20 20:42:38 +0100

    validation and test for sum method

--- index.js
+++ index.js
@@ -10,8 +10,24 @@ class CoolClass{
 	* @return sum of all num
 	*/
 	sum(arr_of_numbers) {
+
+		var total = 0; 
 		// Fail if input is not an array
-		if( !Array.isArray(arr_of_numbers) ) return "arrays only";
+		if( !Array.isArray(arr_of_numbers) ) 
+			return "arrays only";
+
+		// Ensure array contains only numbers
+		for( var i = 0; i < arr_of_numbers.length; i++){
+			if(typeof arr_of_numbers[i] !== "number" )
+				return "only numbers in array";
+		}
+
+		for(var j = 0; j < arr_of_numbers.length; j++){
+			total += arr_of_numbers[j];
+		}
+
+		return total;
+
 	}
 }
 
--- test/index-spec.js
+++ test/index-spec.js
@@ -5,9 +5,20 @@ var expect = chai.expect
 // Bring in the script
 var coolClass = require("../index")
 
+var myCalculator = new coolClass();
+
 describe("Tests cases for the sum method", function(){
 		it("return an error when anything other than array is passed", function(){
-			var myCalculator = new coolClass();
-			expect( myCalculator.sum([1, 2, 3]) ).to.equal("arrays only");
+			expect( myCalculator.sum(1, 2, 3) ).to.equal("arrays only");
+		})
+
+		it("array element must be numbers only", function(){
+			var badArray = ["1", 2, 3, " "]
+			expect( myCalculator.sum(badArray) ).to.equal("only numbers in array");
+		})
+
+		it("When invoke with [1, 2, 3] should return 6", function(){
+			var arr = [1, 2, 3]
+			expect( myCalculator.sum(arr) ).to.equal(6);
 		})
 })
